d193ef359c172bb935ddf76af7a3c4cce61fa764
Luke Sahula <lukas.sahula@icloud.com>
2025-05-22 10:22:23 +0200

    Split create article page and form

---
+++ src/app/components/ArticleForm.tsx
@@ -0,0 +1,63 @@
+"use client";
+import dynamic from "next/dynamic";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import * as z from "zod";
+
+const MarkdownInput = dynamic(() => import("@/components/MDXEditor"), {
+  ssr: false,
+});
+
+const FormSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title must be at least 1 characters.",
+  }),
+  content: z.string().min(1, {
+    message: "Content must be at least 1 character.",
+  }),
+});
+
+type FormData = z.infer<typeof FormSchema>;
+
+const CreateForm = () => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(FormSchema) });
+
+  const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <label htmlFor="title" className="font-bold">
+        Title
+      </label>
+      <input
+        placeholder="My First Article"
+        {...register("title")}
+        className="py-2 px-4 text-foreground bg-background rounded-[8px] border-[1px] border-foreground"
+      />
+      {errors.title && <span>{errors.title.message}</span>}
+      <label htmlFor="content" className="font-bold">
+        Content
+      </label>
+      <Controller
+        name="content"
+        control={control}
+        rules={{ required: "Content is required" }}
+        render={({ field, fieldState }) => (
+          <div>
+            <MarkdownInput markdown={field.value} onChange={field.onChange} />
+            {fieldState.error && <p>{fieldState.error.message}</p>}
+          </div>
+        )}
+      />
+    </form>
+  );
+};
+
+export default CreateForm;
--- src/app/create-article/page.tsx
+++ src/app/create-article/page.tsx
@@ -1,28 +1,10 @@
 "use client";
-import dynamic from "next/dynamic";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { ReactElement } from "react";
-import { useForm, Controller, SubmitHandler } from "react-hook-form";
-import * as z from "zod";
+import CreateForm from "@/components/ArticleForm";
 
-const MarkdownInput = dynamic(() => import("@/components/MDXEditor"), {
-  ssr: false,
-});
-
-const FormSchema = z.object({
-  title: z.string().min(1, {
-    message: "Title must be at least 1 characters.",
-  }),
-  content: z.string().min(1, {
-    message: "Content must be at least 1 character.",
-  }),
-});
-
-type FormData = z.infer<typeof FormSchema>;
-
-export default function CreatePage() {
+const CreatePage = () => {
   const { status } = useSession({
     required: true,
     onUnauthenticated() {
@@ -30,54 +12,11 @@ export default function CreatePage() {
     },
   });
 
-  const {
-    control,
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({ resolver: zodResolver(FormSchema) });
-
-  const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
-    console.log(data);
-  };
-
   let PageContent: ReactElement;
   if (status === "loading") {
     PageContent = <p>Loading authentication status</p>;
   } else {
-    PageContent = (
-      <>
-        <h1 className="font-bold text-2xl">Create new article</h1>
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-          <label htmlFor="title" className="font-bold">
-            Title
-          </label>
-          <input
-            placeholder="My First Article"
-            {...register("title")}
-            className="py-2 px-4 text-foreground bg-background rounded-[8px] border-[1px] border-foreground"
-          />
-          {errors.title && <span>{errors.title.message}</span>}
-          <label htmlFor="content" className="font-bold">
-            Content
-          </label>
-          <Controller
-            name="content"
-            control={control}
-            rules={{ required: "Content is required" }}
-            render={({ field, fieldState }) => (
-              <div>
-                <MarkdownInput
-                  markdown={field.value}
-                  onChange={field.onChange}
-                />
-                {fieldState.error && <p>{fieldState.error.message}</p>}
-              </div>
-            )}
-          />
-        </form>
-      </>
-    );
+    PageContent = <CreateForm />;
   }
 
   return (
@@ -85,4 +24,6 @@ export default function CreatePage() {
       {PageContent}
     </div>
   );
-}
+};
+
+export default CreatePage;
