0b41f0a1bfc7e59adb3e8a2c28909103b54e84d3
Luke Sahula <lukas.sahula@icloud.com>
2025-05-22 11:14:27 +0200

    Add my articles page placeholder

---
+++ src/app/my-articles/page.tsx
@@ -0,0 +1,14 @@
+const MyArticles = () => {
+  return (
+    <div className="flex mt-40 justify-center items-center flex-col gap-4">
+      <h1 className="font-bold text-2xl">My Articles</h1>
+      <ul>
+        <li>Article 1</li>
+        <li>Article 2</li>
+        <li>Article 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default MyArticles;
